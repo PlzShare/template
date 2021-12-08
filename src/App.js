bdf9d3798548a2552559453644bcf72b9a8623a0
onion237 <ssafy1121@gmail.com>
2021-12-08 20:10:51 +0900

    axios

--- src/App.js
+++ src/App.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import DashboardLayout from './layouts/DashboardLayout';
 import './assets/scss/styles.scss';
-
+import axios from 'axios';
 export default function App() {
+  // axios.defaults.baseURL = '/api'
+  // axios
+  //   .get('/workspaces/1/channels/1/documents')
+  //   .then((response) => {
+  //     console.dir(response)
+  //   })
   // fetch('/api/workspaces/1/channels/1/documents')
   return (
     <BrowserRouter>
