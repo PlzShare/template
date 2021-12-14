4c975d3450832cd024ea7a0e9addda84e9b32f57
pocpoc0202 <pocpoc0202@naver.com>
2021-12-14 13:35:34 +0900

    dk

--- src/layouts/Dashboard.js
+++ src/layouts/Dashboard.js
@@ -26,7 +26,9 @@ const Dashboard = () => {
       const deleteNo = e.target.id;
       await axios.delete(`/workspaces/workspace-users/${testUserNo}/${deleteNo}`);
 
-      console.log("삭제한 워크스페이스 번호:" + deleteNo);
+      console.log("==============================")
+      console.log("현재 테스트 넘버는 " + testUserNo + "입니다. 현재 이 계정은 Admin이므로, 원칙상으로는 워크스페이스 방을 떠날 수 없지만 일단 기능구현을 보여주기 위해 떠날 수 있게 해두었습니다.")
+      console.log(testUserNo + "번이 워크스페이스를 떠났어요~! 떠난 워크스페이스 번호는" + deleteNo + "번이랍니다? ㅎ");
       setNames([...(names.filter(name => name.no != deleteNo))])
     }
   }
